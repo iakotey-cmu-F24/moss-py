58e1b349ac2ca323b1c92fdf330df3cfa77f1a56
Ian Akotey <ianakotey@gmail.com>
2022-08-13 22:49:08 +0000

    Added moss parameter ABC

--- src/moss_client.py
+++ src/moss_client.py
@@ -5,6 +5,13 @@ from enum import Enum
 from pathlib import Path
 from typing import Iterator
 
+# ! Unnecessary once Python 3.11 is released
+# ? `pip install typing-extensions` for now
+from typing_extensions import Self
+
+
+PathType = Path | str
+
 
 class MossLanguage( str, Enum ):
 
@@ -33,3 +40,51 @@ class MossLanguage( str, Enum ):
     A8086 = 'a8086'
     JAVASCRIPT = 'javascript'
     PLSQL = 'plsql'
+
+
+class MossParams( ABC ):
+
+    ''' Abstract class representing a moss parameter. 
+        Allows the user to configure Moss parameters and submit queries
+        Implements most shared behavior expected of subclasses
+    '''
+
+    def __init__( self ) -> None:
+        super().__init__()
+        self.__comment: str = str( datetime.today() )
+        self.__experimental_mode: bool = False
+        self.__language: MossLanguage = MossLanguage.C
+        self.__max_ignore_threshold = 10
+        self.__match_number = 250
+
+    def set_language( self, language: MossLanguage ) -> Self:
+        '''Set the language (-l) parameter to be used in the MOSS query'''
+        self.__language = language
+        return self
+
+    def set_experimental( self, experimental_flag: bool ) -> Self:
+        '''Set the experimental flag (-x)'''
+        self.__experimental_mode = experimental_flag
+        return self
+
+    def set_comment( self, comment: str ) -> Self:
+        '''Set the comment (-c) string to be used in the MOSS query'''
+        self.__comment = comment
+        return self
+
+    def __str__( self ):
+        return f'moss -c {self.__comment} -l {self.__language}'                 \
+               f' -m {self.__max_ignore_threshold} -n {self.__match_number}'    \
+               f'{" -x" if self.__experimental_mode else ""}'                                    \
+               f' {" ".join(("-b " + str(file) for file in self.base_files()))}'        \
+               f' {" ".join((str(file) for file in self.submission_files()))}'
+
+    @abstractmethod
+    def base_files( self ) -> Iterator[ PathType ]:
+        pass
+
+    @abstractmethod
+    def submission_files( self ) -> Iterator[ PathType ]:
+        pass
+
+
